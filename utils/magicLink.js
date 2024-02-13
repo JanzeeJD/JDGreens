6a3969d793829f6e2e36ad236d9740317f7a0fc1
Jancy Daniel <jancyjaya12@gmail.com>
2024-02-13 20:53:45 +0530

    feat: add magic link functions

---
+++ utils/magicLink.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+export function createMagicLink(baseURL, userId) {
+  // Set the payload with userId
+  const payload = {
+    userId,
+  };
+
+  // todo: Put the secret in process.env
+  // Sign the payload with a secret key, add expiration time, and create the JWT token
+  const token = jwt.sign(payload, "jancy's little secret", { expiresIn: '5m' });
+
+  // Construct the magic link by appending the token to the baseURL
+  const magicLink = `${baseURL}/${token}`;
+
+  return magicLink;
+}
+
+export function verifyMagicLink(token) {
+  try {
+    // Verify the token using the secret key
+    const decoded = jwt.verify(token, "jancy's little secret");
+    return decoded.userId;
+
+  } catch (error) {
+    return null;
+  }
+}
