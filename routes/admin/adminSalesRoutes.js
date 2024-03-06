80c1668da394e853ed608c0c3fecd3c5af48a7ff
Jancy Daniel <jancyjaya12@gmail.com>
2024-03-06 12:19:19 +0530

    feat: update admin routes

--- routes/admin/adminSalesRoutes.js
+++ routes/admin/adminSalesRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { isAdmin } from '../../middlewares/adminAuthMiddleware.js';
-import { GenerateSalesReportAsPDF, GetSalesReportPage } from '../../controllers/admin/adminSalesController.js';
+import { GenerateSalesReport, GetSalesReportPage } from '../../controllers/admin/adminSalesController.js';
 
 const router = Router();
 router.use(isAdmin);
 
 router.get("/", GetSalesReportPage);
-router.post("/:format", GenerateSalesReportAsPDF);
+router.post("/:format", GenerateSalesReport);
 
 export default router;
